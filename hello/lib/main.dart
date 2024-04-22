8344d1e295625480d59fe40d24fcfc3932b8898c
Subhikshaa23 <210701264@rajalakshmi.edu.in>
2024-04-22 14:11:00 +0530

    Try asset image inclusion

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -27,7 +27,9 @@ class _HomeState extends State<Home> {
           //   width: 100,
           //   color: Colors.pinkAccent,
           // ),
-          Image(image: NetworkImage("https://th.bing.com/th?id=OIP.NfRlVuapZjQd2aaoNUTYRQHaHa&w=250&h=250&c=8&rs=1&qlt=30&o=6&pid=3.1&rm=2")),
+          // Image(image: NetworkImage("https://th.bing.com/th?id=OIP.NfRlVuapZjQd2aaoNUTYRQHaHa&w=250&h=250&c=8&rs=1&qlt=30&o=6&pid=3.1&rm=2")),
+          Image(image: AssetImage("assets/Profile.jpg")),
+
           // Container(
           //   height: 100,
           //   width: 100,
